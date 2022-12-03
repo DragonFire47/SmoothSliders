4e33dc02feaf35386e3c5c250d7a8866bbc543a3
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2022-12-02 21:42:50 -0800

    Updated Keybinds to use PML IKeyBind Interface

--- SmoothSliders/Mod.cs
+++ SmoothSliders/Mod.cs
@@ -1,13 +1,20 @@
 ﻿using PulsarModLoader;
+using PulsarModLoader.Keybinds;
 
 namespace SmoothSliders
 {
-    public class Plugin : PulsarMod
+    public class Mod : PulsarMod, IKeybind
     {
         public override string HarmonyIdentifier()
         {
             return "pngun.SmoothSliders";
         }
+
+        public void RegisterBinds(KeybindManager manager)
+        {
+            manager.NewBind("Toggle Smooth Sliders", "toggle_smoothsliders", "Basics", "k");
+        }
+
         public override string Author => "pngun";
         public override string ShortDescription => "Allows to set power levels on Engineering Screen more precisely";
         public override string Name => "SmoothSliders";
